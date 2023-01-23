6772a89dede27546552275aaa7fb59022aacddd8
mohrefaldorachman <mohrefaldorachman@gmail.com>
2023-01-23 15:34:47 +0700

    change ul li position

--- src/js/settingsPage/main.js
+++
@@ -1,6 +0,0 @@
-$("#meta-title-input").keyup(function (e) {
-  $("#meta-title-input-help").html(this.value.length + " / " + 60);
-});
-$("#meta-description-input").keyup(function (e) {
-  $("#meta-description-input-help").html(this.value.length + " / " + 160);
-});
